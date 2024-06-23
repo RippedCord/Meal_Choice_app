68ac36fab1cf8e8ba07f41f2846778ff7b2a5946
RippedCord <kieran.markell@hotmail.co.uk>
2024-06-23 16:47:13 +0100

    the very basic program is now working

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 import csv
 import os
+import pandas
+import random
+
 
 class meal:
 
@@ -32,6 +35,63 @@ def add_meal():
             writer.writerow({"Name":new_meal.name, "Size": new_meal.size, "attributes":new_meal.attributes})
     
 
-def meal_plan():
-    days = int(input("how many days do you need meals for?"))
     
+def meal_remove():
+    data = pandas.read_csv("meal_list.csv")
+    data = data.reset_index = True
+    print(data.to_string)
+    while True:
+        meal_num = int(input("what is the index number of the meal you would like to remove?"))
+        print(data.iloc[meal_num])
+        con = input("Is this the correct meal? Y/N ").upper()
+        if con == "Y":
+            data.drop(index=[meal_num])
+            break
+        else:
+            if input("Return to the main menu? Y/N").upper() == "Y":
+                break
+            else:
+                continue
+
+
+
+def meal_plan():
+    # Asks how many of each size meal is needed
+    meals = input("how many meals do you need? S,M,L ")
+    meals = meals.split()
+    meals_list = [int(x) for x in meals]
+    data = pandas.read_csv("meal_list.csv")
+
+    final_smeal= []
+    final_mmeal= []
+    final_lmeal= []
+
+    # Gets all the meals in vars by size
+    smeal = data[data["Size"] == "S"]
+    mmeal = data[data["Size"] == "M"]
+    lmeal = data[data["Size"] == "L"]
+
+    # Gets the correct number of meals for each size
+    for _ in range(meals_list[0]):
+        final_smeal.append(random.choice(smeal.to_dict("records")))
+    
+    for _ in range(meals_list[1]):
+        final_mmeal.append(random.choice(mmeal.to_dict("records")))
+    
+    for _ in range(meals_list[2]):
+        final_lmeal.append(random.choice(lmeal.to_dict("records")))
+
+    # Prints all the meals in the plan
+    ### maybe add an index for changing a meal for the user?
+    print("Small meals:")
+    for meal in final_smeal:
+        
+        print(meal["Name"])
+    
+    print("Medium meals:")
+    for meal in final_mmeal:
+        print(meal["Name"])
+    
+    print("Large meals:")
+    for meal in final_lmeal:
+        print(meal["Name"])
--- main_loop.py
+++ main_loop.py
@@ -5,10 +5,10 @@ while True:
     loop_num = input("please type the number of the opperation you'd like to perform: \n 1. add a new meal \n 2. remove or edit a meal \n 3. create a new set of meals for the week")
     if loop_num =="1":
         functions.add_meal()
-        # function for adding a new meal
-    #elif loop_num == "2":
-        # function for removing or editing a meal
-    #elif loop_num == "3":
-        #function for making a meal plan
+        
+    elif loop_num =="2":
+        functions.meal_remove()
+    elif loop_num == "3":
+        functions.meal_plan()
     else:
         continue
